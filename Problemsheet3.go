61fa037758ec5349a253d8d1aea8d7830e8465f6
davidclarke1995 <davidclarke1995is@gmail.com>
2017-11-16 17:35:03 +0000

    Lab

--- Problemsheet3.go
+++ Problemsheet3.go
@@ -19,25 +19,48 @@ import (
 
         //fmt.Println("Please message me")
         //fmt.Scanf("%s", &userInput)
-		userInput :=inputStr
+		//userInput :=inputStr
 
-        //matching the word father
-        if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, userInput); matched {
-            //returning the string below
-            return "Why don’t you tell me more about your father?"
+		userInput := inputStr
+		pattern := `(?!).*\bfather\b.*`
+		pattern2 := []string{`.*i am(.*)`, `.*I AM(.*)`, `.*I'm(.*)`, `.*i'm(.*)`, `.*im(.*)`, `.*I am(.*)`}
+		output := "Why dont you tell me more about your father?"
+		response := ""
+		rand.Seed(time.Now().UTC().UnixNano())
 
-        }
 
-        re := regexp.MustCompile(`.*I am(.*)`)
-        if re.MatchString(userInput){
-        return re.ReplaceAllLiteralString(userInput, "How do you know you are $1")
-        }
 
-        answers := []string{//responses
+		answers := []string{//responses
         "I’m not sure what you’re trying to say. Could you explain it to me?",
         "How does that make you feel?",
         "Why do you say that?",
         }
+
+
+        //matching the word father
+        if matched, _ := regexp.MatchString(pattern, userInput); matched {
+        //returning the string below
+        response = "input :" + userInput + "\noutput :" + output
+
+		} else {
+		response = "input :" + userInput + " \noutput :" + answers[rand.Intn(len(answers))]
+		}
+
+		for _, checkPattern := range pattern2 {
+			re := regexp.MustCompile(checkPattern)
+			if re.MatchString(userInput) {
+				match := re.ReplaceAllString(userInput, "How do you know you are $1?")
+				response = "input : " + userInput + " \noutput : " + match
+				return response
+			} //if re.MatchString
+		} //for pattern2
+
+		return response
+
+        //re := regexp.MustCompile(`.*I am(.*)`)
+        //if re.MatchString(userInput){
+       // return re.ReplaceAllLiteralString(userInput, "How do you know you are $1")
+       //}
 
         //returning the single string response
     return answers[rand.Intn(len(answers))]
@@ -45,21 +68,31 @@ import (
 
 
 func main() {
+	
+	userInput:= [] string{
+		"People say I look like both my mother and father.",
+		"Father was a teacher.",
+		"I was my father’s favourite.",
+		"I'm looking forward to the weekend.",
+		"My grandfather was French!",
+		"I am happy.",
+		"I am not happy with your responses.",
+		"I am not sure that you understand the effect that your questions are having on me.",
+		"I am supposed to just take what you’re saying at face value?",
+	}
+	
 	rand.Seed(time.Now().UTC().UnixNano())//get a random number
 	//reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Hello, how are you?")
+	//fmt.Println("Hello, how are you?")
 
 //	userInput, _ := reader.ReadString('\n')
 //	fmt.Scanf("%s", &userInput)
-userInput:= [] string{
- "I am happy",
- "I am a mong",
-}
+
 
 //userInput:= "I am happy"
-elizaOutput:= ElizaResponse(userInput[1]);
+elizaOutput:= ElizaResponse(userInput[rand.Intn(len("userInput"))]);
 
 
 
-	fmt.Println("\nuser: "+userInput[1]+"\neliza:"+elizaOutput)
-}
+	fmt.Println(elizaOutput)
+}//ElizaResponse
